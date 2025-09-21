ad89892797e96ca0932f4f94131b5564c81747a8
Kir <kir.allyanov@gmail.com>
2025-09-21 21:38:36 +0300

    commit

--- main.py
+++ main.py
@@ -1,55 +1,94 @@
 import time
-
+import random
 
 class Robot:
 
-
     def __init__(self):
-        pass
+        self.local_map = []
 
-    def moving(self, p_x, p_y, local_map):
+    def moving(self, s_x, p_y):
+        local_map = self.look_around()
+        delta_x = random.randint(-1,1)
+        delta_y = random.randint(-1,1)
         d_x = 0
-        d_y = 1
-        return d_x, d_y
+        d_y = 0
+        return d_x+delta_x, d_y+delta_y
 
-# Роботу на основании полученных данных прнимать решение, куда идти
+
+    def look_around(self):
+        self.local_map = []
+        global_map = world1.map
+        robot = world1.get_robot_list()
+        for y in range(len(global_map)):
+            row = []
+            row_c = []
+            for x in range(len(global_map[y])):
+                if y in range(robot[0][1]-1, robot[0][1]+2) and x in range(robot[0][2]-1, robot[0][2]+2):
+                    if y == robot[0][1] and x == robot[0][2]:
+                        row.append(1)
+                        row_c.append([x,y])
+                    else:
+                        row.append(global_map[y][x])
+                        row_c.append([x,y])
+            if len(row) != 0:
+                self.local_map.append(row)
+                self.local_map.append(row_c)
+        return self.local_map
+
+
+# Робот на основании полученных данных прнимает решение, куда идти
 
 
 class World:
 
-    def __init__(self, width, lenght):
-        self.map = [[0] * width for _ in range(lenght)]
+    def __init__(self, width, length):
+        self.map = [[0] * width for _ in range(length)]
         self.robot_list = []
+        self.obstacle = []
 
-    def add_robot(self, x,y):
-         robot = [Robot(),x,y]
-         self.robot_list.append(robot)
+    def add_robot(self, x, y):
+        robot = [Robot(), x, y]
+        self.robot_list.append(robot)
 
     def draw_map(self):
-        for row in self.map:
-            print(*row)
+        c_y = self.robot_list[0][1]
+        c_x = self.robot_list[0][2]
+        for y in range(len(self.map)):
+            for x in range(len(self.map[y])):
+                if x == c_x and y == c_y:
+                    print(1, end=' ')
+                else:
+                    print(self.map[y][x], end=' ')
+            print()
 
     def get_robot_list(self):
-        print(self.robot_list)
+        return self.robot_list
 
     def step(self):
         for r in self.robot_list:
+            print(*r[0].look_around())
             x = r[1]
             y = r[2]
-            d_x, d_y = r[0].moving(x, y, None)
-            r[1] += d_x
-            r[2] += d_y
+            d_x, d_y = r[0].moving(x, y)
+            if len(self.map)-1 > y and len(self.map[0])-1 > x: # Сравниваем координаты робота и размеры Мира, чтобы не выйти за его пределы
+                r[1] += d_x
+                r[2] += d_y
+            else:
+               exit()
 
-# реализовать, чтобы он не убегал за края карты, в рамках step
+
+# реализовать, чтобы он не убегал за края карты, в рамках step (done)
 # И чтобы робот не втыкался в препятсвия
-# Передавать роботу локальную карту (маленький кусочек)
+# Передавать роботу локальную карту (маленький кусочек) (done)
 
 world1 = World(10, 10)
-world1.add_robot(5,5)
-world1.draw_map()
+world1.add_robot(5, 5)
 world1.get_robot_list()
-for i in range(3):
+print()
+for i in range(7):
+    print(world1.get_robot_list())
+    world1.draw_map()
     world1.step()
     world1.get_robot_list()
-
-
+    print()
+    time.sleep(1)
