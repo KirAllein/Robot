8830552c83741fc64b9d6b617d72e26f50100028
Kir <kir.allyanov@gmail.com>
2025-10-15 08:59:04 +0300

    Модернизировал алгоритм движение робота

--- main.py
+++ main.py
@@ -6,22 +6,35 @@ class Robot:  # Класс робота
     def __init__(self):
         self.local_map = []
         self.direction = 0
+        self.robot_met_obstacle = False
+        self.obstacle_coordination = [-1,-1]
 
     def moving(self, global_x, global_y, local_map):  # функция движения робота
         directions = [[0,-1],[-1,0],[0,1],[1,0]]
+        if self.robot_met_obstacle:
+            if global_x == self.obstacle_coordination[1] and global_y == self.obstacle_coordination[0]:
+                return [0,0]
+        if self.robot_met_obstacle:
+            self.direction -= 1
+            if self.direction < 0:
+                self.direction = 3
         x_ch = directions[self.direction][0] + 1
         y_ch = directions[self.direction][1] + 1
-        if local_map[y_ch][x_ch] != 0 :
+        while local_map[y_ch][x_ch] != 0:
+            if not self.robot_met_obstacle:
+                self.obstacle_coordination = [global_y,global_x]
+                self.robot_met_obstacle = True
             self.direction += 1
-        if self.direction > 3:
-            self.direction = 0
+            if self.direction > 3:
+                self.direction = 0
+            x_ch = directions[self.direction][0] + 1
+            y_ch = directions[self.direction][1] + 1
         print(self.direction)
         print('___________________________________________________')
         for row in local_map:
             print(*row)
         print(directions[self.direction])
         return directions[self.direction] # возвращаем измененные значения координат на соответствующую дельту
-
 class World:  # Класс Мира
 
     def __init__(self, width, length, obstacle=[]):
@@ -40,17 +53,17 @@ class World:  # Класс Мира
                         world[y][x] = 'X'  # препятствие у нас обозначено буквой х
 
 
-        world[2][3] = 'X'
-        world[2][4] = 'X'
-        world[2][5] = 'X'
-        world[2][6] = 'X'
         world[2][2] = 'X'
-        # world[6][3] = 'X'
-        # world[7][3] = 'X'
-        # world[4][5] = 'X'
-        # world[5][5] = 'X'
-        # world[6][5] = 'X'
-        # world[7][5] = 'X'
+        world[3][2] = 'X'
+        world[4][2] = 'X'
+        world[5][2] = 'X'
+        world[6][2] = 'X'
+        world[2][4] = 'X'
+        world[3][4] = 'X'
+        world[4][4] = 'X'
+        world[5][4] = 'X'
+        world[6][4] = 'X'
+        world[2][3] = 'X'
 
 
         return world
@@ -64,15 +77,16 @@ class World:  # Класс Мира
 
 
     def draw_map(self):  # Класс рисования Мира
-        c_y = self.robot_list[0][1]  # координата робота у
-        c_x = self.robot_list[0][2]  # координата робота х
-        for y in range(len(self.map)):
-            for x in range(len(self.map[y])):
-                if x == c_x and y == c_y:  # если координаты сошлись на координатах робота, значит здесь робот
-                    print('R', end=' ')
-                else:
-                    print(self.map[y][x], end=' ')  # в остальных случаях рисуем просто поля Мира
-            print()
+        for i, j in enumerate(self.robot_list):
+            c_y = self.robot_list[i][1]  # координата робота у
+            c_x = self.robot_list[i][2]  # координата робота х
+            for y in range(len(self.map)):
+                for x in range(len(self.map[y])):
+                    if x == c_x and y == c_y:  # если координаты сошлись на координатах робота, значит здесь робот
+                        print('R', end=' ')
+                    else:
+                        print(self.map[y][x], end=' ')  # в остальных случаях рисуем просто поля Мира
+                print()
 
     def get_robot_list(self):  # функция, чтобы получить список всех роботов
         return self.robot_list
@@ -122,8 +136,9 @@ class World:  # Класс Мира
         return robot_map
 
 
-world1 = World(10, 5)
-world1.add_robot(4,4, world1)
+world1 = World(10, 10)
+world1.add_robot(9,4, world1)
+world1.add_robot(9,9, world1)
 world1.get_robot_list()
 print()
 while True:
@@ -135,33 +150,8 @@ while True:
     time.sleep(2)
 
 # Список замечаний:
-# 1. Робот добавляется на поле препятствия ( поправил)
-# 2. Код останавливается, когда робот попадает на край препятствия (поправил)
-# 3. Отображается только 1 робот, остальные роботы не отображаются
-# 3/1. Нельзя отрисовать Мир без робота
-# 4. Препятствие захардкожена в классе Мир. (Fixed)
-# 5. Нельзя создать Мир без препятствий (Fixed)
-# 6. Локальная карта передается роботу из класса Мир, и робот довольствуется только тем, что ему передано (Done)
-# 7. Ошибка при приближении к границе Мира (Done)
-# 8. Если доходим до края Мира, это должно восприниматся как препятствие (Done)
-# 9. Проверка наличия препятствия ориентирует только на латинскую буквы X (Done)
-# 10. Робот не видит дыру в заборе
-# 11. Научить робота ходить по диагонале
-# 12. Робот проходит сквозь препятствие слева
+# Поправить отрисовку нескольких роботов
+# аучить робота ходить по диагонале
 
 
-# 13. Робот проходит сквозь препятствие, если движется по краю (fixed)
-# 14. Робот всегда должен знать свои глобальные координаты
-# 15. Робот должен двигаться по препятставию в форме П
-
-
-# 16. Сделать правильное заполнение локальной карты, чтобы всегда три линиии (Done)
-# 17. поправить moving. Всегда по локальной карте смотрим, доступность нужного направления и если оно свободно, то двигаемся в нем, если нет
-# то перебираем следующие. текущее направление и сначала проверяем свободно ли его. Задача - сохранять текущее направление
-# если текущее направление недоступно, то пытаемся повернуть
-# 18. При обходе препятствия запоминаем глобальные координаты, когда в первый раз встретил препятствие
-
-
-# Вернуться к исходному заданию и подумать как это реализовать
-
-
+# Распутать Х и У
